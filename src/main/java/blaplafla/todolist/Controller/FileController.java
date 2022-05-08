d2746c2e920369f1d1a5e5e4bcc69467a6979461
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-05-08 17:58:00 +0700

    add FileController

---
+++ src/main/java/blaplafla/todolist/Controller/FileController.java
@@ -0,0 +1,75 @@
+package blaplafla.todolist.Controller;
+
+import blaplafla.todolist.Modals.ListTask;
+
+import java.io.*;
+
+public class FileController {
+    private File userfile;
+    private boolean hasFile = false;
+    private ListTask listTask;
+    private FileInputStream fileInputStream;
+    private ObjectInputStream objectInputStream;
+    private FileOutputStream fileOutputStream;
+    private ObjectOutputStream objectOutputStream;
+    private static FileController controller;
+
+    private FileController() {
+    }
+
+    public boolean hasFile() {
+        return userfile != null;
+    }
+
+    public int setFile(File userfile) {
+        this.userfile = userfile;
+        try {
+            fileInputStream = new FileInputStream(userfile);
+            objectInputStream = new ObjectInputStream(fileInputStream);
+            fileOutputStream = new FileOutputStream(userfile);
+            objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            hasFile = true;
+            return 1;
+        } catch (FileNotFoundException e) {
+            return 2;
+        } catch (IOException e) {
+            return 3;
+        }
+    }
+
+    public ListTask getListTask() {
+        return listTask;
+    }
+
+    public int importListTask() {
+        if (hasFile) {
+            try {
+                listTask = (ListTask) objectInputStream.readObject();
+                return 1;
+            } catch (IOException e) {
+                return 3;
+            } catch (ClassNotFoundException e) {
+                return 4;
+            }
+        }
+        return 2;
+    }
+    public int exportListTask() {
+        if (hasFile) {
+            try {
+                objectOutputStream.writeObject(listTask);
+                return 1;
+            } catch (IOException e) {
+                return 3;
+            }
+        }
+        return 2;
+    }
+
+    public static FileController getInstance() {
+        if (controller == null) {
+            controller = new FileController();
+        }
+        return controller;
+    }
+}
