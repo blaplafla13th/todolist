e3cc4459bf8b4a69854a8de716c0f688ee3c6945
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-05-08 23:57:05 +0700

    add SetLanguage view

---
+++ src/main/java/blaplafla/todolist/views/cli/SetLanguage.java
@@ -0,0 +1,28 @@
+package blaplafla.todolist.views.cli;
+
+import blaplafla.todolist.controllers.DictionaryController;
+
+import java.util.Scanner;
+
+public class SetLanguage {
+    public static void main(String[] args) {
+        DictionaryController d = DictionaryController.getInstance();
+        System.out.println(d.label("current-lang")+d.getDictionary());
+        System.out.println(d.label("set-lang"));
+        int code = DictionaryController.getInstance().getLanguageList();
+        if (code != 100){
+            System.out.println(d.errorExplain(code));
+        }
+        System.out.println(d.label("set-lang-term"));
+        Scanner input = new Scanner(System.in);
+        int i = 0;
+        try{
+            i = Integer.parseInt(input.nextLine());
+        } catch (NumberFormatException e){
+            System.out.println(d.errorExplain(500));
+        }
+        d.setDictionary(i);
+        System.out.println(d.label("current-lang")+d.getDictionary());
+
+    }
+}
