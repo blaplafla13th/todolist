39c23c8d8d0f7502c6c1c0722492df6ec6d88bb3
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-05-08 17:57:45 +0700

    add Task

---
+++ src/main/java/blaplafla/todolist/Modals/MotherTask.java
@@ -0,0 +1,29 @@
+package blaplafla.todolist.Modals;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class MotherTask extends Task implements Serializable, Comparable<Task> {
+    private SimpleArrayList<Task> undoneSubTask;
+    private SimpleArrayList<Task> doneSubTask;
+
+    public MotherTask(String title, String description, Date deadline, int priority) {
+        super(title, description, deadline, priority);
+        undoneSubTask = new SimpleArrayList<>();
+        doneSubTask = new SimpleArrayList<>();
+    }
+
+    public SimpleArrayList<Task> getSubTask() {
+        return undoneSubTask;
+    }
+
+    public void doneSubtask(int id) {
+        doneSubTask.add(undoneSubTask.get(id));
+        undoneSubTask.removeIndex(id);
+    }
+
+    public void undoneSubtask(int id) {
+        undoneSubTask.add(doneSubTask.get(id));
+        doneSubTask.removeIndex(id);
+    }
+}
---
+++ src/main/java/blaplafla/todolist/Modals/Task.java
@@ -0,0 +1,74 @@
+package blaplafla.todolist.Modals;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Task implements Serializable, Comparable<Task> {
+    private String title;
+    private String description;
+    private Date deadline;
+    private int priority;
+
+
+    public Task(String title, String description, Date deadline, int priority) {
+        this.title = title;
+        this.deadline = deadline;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public long leftTime() {
+        return deadline.getTime() - System.currentTimeMillis(); //millisecond
+    }
+
+    @Override
+    public int compareTo(Task task) {
+        int temp = Long.compare(this.leftTime() / priority, task.leftTime() / priority);
+        return temp == 0 ? Long.compare(this.leftTime(), task.leftTime()):temp;
+    }
+
+    public String prettyTimer() {
+        long leftTime = leftTime();
+        leftTime = leftTime / 1000; //convert to second
+        long day = leftTime / 24 / 60 / 60;
+        leftTime = leftTime - day * 24 * 60 * 60;
+        long hour = leftTime / 60 / 60;
+        leftTime = leftTime - hour * 60 * 60;
+        long minute = leftTime / 60;
+        long second = leftTime - minute * 60;
+        return String.format("còn lại %d ngày %d giờ %d phút %d giây", day, hour, minute, second);
+    }
+
+}
