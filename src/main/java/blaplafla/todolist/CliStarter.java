00f4aa3576f8b5c65e3736558ac02c4dbcdbfbf3
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-05-11 11:15:20 +0700

    create router for cli

--- src/main/java/blaplafla/todolist/CliStarter.java
+++ src/main/java/blaplafla/todolist/CliStarter.java
@@ -1,4 +1,13 @@
 package blaplafla.todolist;
 
+import blaplafla.todolist.controllers.MainController;
+import blaplafla.todolist.views.View;
+import blaplafla.todolist.views.cli.SetLanguage;
+
 public class CliStarter {
+    public static void main(String[] args) {
+        MainController mainController = MainController.getInstance();
+        mainController.setModeCli();
+
+    }
 }
