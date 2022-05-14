e662ad841c6d928ed287f24d040a8f719eb1a8ef
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-05-14 22:49:08 +0700

    CliStarter start from index

--- src/main/java/blaplafla/todolist/CliStarter.java
+++ src/main/java/blaplafla/todolist/CliStarter.java
@@ -1,13 +1,12 @@
 package blaplafla.todolist;
 
 import blaplafla.todolist.controllers.MainController;
-import blaplafla.todolist.views.View;
-import blaplafla.todolist.views.cli.SetLanguage;
 
 public class CliStarter {
     public static void main(String[] args) {
         MainController mainController = MainController.getInstance();
         mainController.setModeCli();
+        mainController.taskController().index();
 
     }
 }
