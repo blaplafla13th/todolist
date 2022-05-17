db26c4e1f1cbcf8d6b705cbfce6933e86adb31ef
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-05-17 21:40:13 +0700

    add DoneCli

---
+++ src/main/java/blaplafla/todolist/views/cli/DoneCli.java
@@ -0,0 +1,102 @@
+package blaplafla.todolist.views.cli;
+
+import blaplafla.todolist.controllers.DictionaryController;
+import blaplafla.todolist.controllers.FileController;
+import blaplafla.todolist.controllers.MainController;
+import blaplafla.todolist.controllers.TaskController;
+import blaplafla.todolist.models.datastructures.SimpleArrayList;
+import blaplafla.todolist.models.task.ListTask;
+import blaplafla.todolist.models.task.MotherTask;
+import blaplafla.todolist.models.task.Task;
+import blaplafla.todolist.request.RequestValidation;
+import blaplafla.todolist.views.View;
+
+public class DoneCli implements View {
+    int page = 1;
+
+    DictionaryController d = MainController.getInstance().dictionaryController();
+    TaskController t = MainController.getInstance().taskController();
+    FileController f = MainController.getInstance().fileController();
+    RequestValidation r = MainController.getInstance().input();
+    SimpleArrayList<Task> tasks;
+    boolean using = true;
+    ListTask listTask = MainController.getInstance().listTask();
+
+    @Override
+    public void run() {
+        while (using) {
+            if (page > max_page) {
+                page = 1;
+            }
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.println(d.label("done-list-name") + listTask.getUsername());
+            tasks = t.paginate(listTask.getDone(), 3, page);
+            int i = 0;
+            if (!tasks.isEmpty())
+                for (Task task : tasks) {
+                    i++;
+                    System.out.println(d.label("-"));
+                    System.out.println(i + ". " + d.label("title") + task.getTitle());
+                    System.out.println(d.label("desc") + task.getDescription());
+                    System.out.println(d.label("subtask-total") + ((MotherTask) task).subTaskSize());
+                    System.out.println(d.label("-"));
+                }
+            else
+                System.out.println(d.label("-"));
+
+            System.out.println(d.label("input-command"));
+            execute(r.input());
+        }
+        using = true;
+    }    int max_page = max_page = t.paginateSize(listTask.getUndone(), 3);
+
+    private void commandlist() {
+        System.out.println(d.label("list command"));
+        System.out.println("next :" + d.label("next-button"));//
+        System.out.println("prev :" + d.label("prev-button"));//
+        System.out.println("toggle :" + d.label("toggle-button"));//
+        System.out.println("delete :" + d.label("delete-button"));//
+        System.out.println("detail :" + d.label("detail-button"));//
+        System.out.println("back :" + d.label("back-button"));//
+    }
+
+    private void execute(String command) {
+        switch (command) {
+            case "help" -> {
+                commandlist();
+                MainController.getInstance().pause();
+            }
+            case "next" -> {
+                if (page < max_page)
+                    page++;
+            }
+            case "prev" -> {
+                if (page > 1)
+                    page--;
+            }
+            case "detail" -> {
+                System.out.print(d.label("index") + ":");
+                t.detailMotherTask((MotherTask) tasks.get(r.inputPositiveInteger(r.input()) - 1));
+            }
+            case "delete" -> {
+                System.out.print(d.label("index") + ":");
+                t.deleteMotherTask((MotherTask) tasks.get(r.inputPositiveInteger(r.input()) - 1));
+                max_page = t.paginateSize(listTask.getUndone(), 3);
+            }
+            case "toggle" -> {
+                System.out.print(d.label("index") + ":");
+                t.toggleMotherTask((MotherTask) tasks.get(r.inputPositiveInteger(r.input()) - 1));
+                max_page = t.paginateSize(listTask.getUndone(), 3);
+            }
+
+            case "back" -> using = false;
+            default -> {
+                System.out.println(d.label("unknown-command"));
+                MainController.getInstance().pause();
+            }
+        }
+    }
+
+
+}
