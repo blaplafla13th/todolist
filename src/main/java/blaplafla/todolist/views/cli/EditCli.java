ac9a615d47b4c60972b2bdddac2094d8c2420f0f
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-05-19 22:29:47 +0700

    add EditCli

---
+++ src/main/java/blaplafla/todolist/views/cli/EditCli.java
@@ -0,0 +1,37 @@
+package blaplafla.todolist.views.cli;
+
+import blaplafla.todolist.controllers.DictionaryController;
+import blaplafla.todolist.controllers.MainController;
+import blaplafla.todolist.controllers.TaskController;
+import blaplafla.todolist.models.task.Task;
+import blaplafla.todolist.request.RequestValidation;
+import blaplafla.todolist.views.View;
+
+import java.util.Date;
+
+public class EditCli implements View {
+    DictionaryController d = MainController.getInstance().dictionaryController();
+    TaskController t = MainController.getInstance().taskController();
+    RequestValidation r = MainController.getInstance().input();
+
+    @Override
+    public void run(Object... params) {
+        if (params[0] instanceof Task task) {
+            System.out.println(d.label("edit-task"));
+            System.out.println(d.label("placeholder-title"));
+            System.out.println(d.label("old-value") + ":" + task.getTitle());
+            String title = r.input();
+            System.out.println(d.label("placeholder-desc"));
+            System.out.println(d.label("old-value") + ":" + task.getDescription());
+            String desc = r.input();
+            System.out.println(d.label("placeholder-deadline"));
+            System.out.println(d.label("old-value") + ":" + t.date(task.getDeadline()));
+            System.out.println(d.label(d.prettyTime(task.prettyTimer())));
+            Date deadline = r.inputTime(null, r.input());
+            System.out.println(d.label("placeholder-priority"));
+            System.out.println(d.label("old-value") + ":" + task.getPriority());
+            int priority = r.inputPositiveInteger(r.input());
+            t.edit(task, title, desc, deadline, priority);
+        } else MainController.getInstance().returnCode(402);
+    }
+}
