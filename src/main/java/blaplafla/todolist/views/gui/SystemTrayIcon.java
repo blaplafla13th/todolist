5fc8c63019922593edf316b813459146e16aec0a
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-05-30 10:14:59 +0700

    fix icon on icon tray too big for showing

--- src/main/java/blaplafla/todolist/views/gui/SystemTrayIcon.java
+++ src/main/java/blaplafla/todolist/views/gui/SystemTrayIcon.java
@@ -8,13 +8,14 @@ import java.awt.*;
 
 public class SystemTrayIcon implements View {
     DictionaryController d = MainController.getInstance().dictionaryController();
+    final SystemTray tray = SystemTray.getSystemTray();
+    final PopupMenu popupMenu = new PopupMenu();
+    final Image image = Toolkit.getDefaultToolkit().getImage(getClass().getResource("icon.png"));
+    final TrayIcon trayIcon = new TrayIcon(image,"To-do List",popupMenu);
 
     @Override
     public void run() {
         if (SystemTray.isSupported()) {
-            SystemTray tray = SystemTray.getSystemTray();
-            Image image = Toolkit.getDefaultToolkit().getImage(getClass().getResource("icon.png"));
-            PopupMenu popupMenu = new PopupMenu();
             MenuItem menuItem0 = new MenuItem(d.label("start-button"));
             menuItem0.addActionListener(actionEvent -> MainController.getInstance().indexView().run());
             MenuItem menuItem1 = new MenuItem(d.label("set-lang-button"));
@@ -25,15 +26,17 @@ public class SystemTrayIcon implements View {
             popupMenu.add(menuItem1);
             popupMenu.addSeparator();
             popupMenu.add(menuItem2);
-            TrayIcon trayIcon = new TrayIcon(image);
-            trayIcon.setToolTip("To-do List");
-            trayIcon.setPopupMenu(popupMenu);
-            trayIcon.setImage(image);
+            trayIcon.setImageAutoSize(true);
             try {
                 tray.add(trayIcon);
             } catch (AWTException e) {
                 MainController.getInstance().returnCode(303);
             }
         }
+    }
+
+    @Override
+    public void run(Object... params) {
+        trayIcon.displayMessage("Notification","blabla",TrayIcon.MessageType.WARNING); // for arlam
     }
 }
